411faa5bf50388fa260f16b634203f3510ded5dd
Francesco Derme <francesco.derme02@gmail.com>
2025-12-31 17:08:40 +0100

    Folder structure

--- include/particle.hpp
+++ include/particle.hpp
@@ -1,4 +1,11 @@
 #ifndef PARTICLE_HPP
 #define PARTICLE_HPP
 
-#endif PARTICLE_HPP
+#include <external/SFML/Graphics.cpp>
+
+class Particle {
+   public:
+    sf::Vector2f position;
+}
+
+#endif PARTICLE_HPP  // PARTICLE_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,8 @@
 // Verlet intehration: r_(n+1) = 2r_n - r_(n-1) + a_n * dt^2
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
